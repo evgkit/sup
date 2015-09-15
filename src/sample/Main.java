6af561bae7080aebad9109ed49b5b83e01c6ab0e
evgkit <kitaev.ea@gmail.com>
2015-09-15 23:33:22 +0300

    Add few nodes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,23 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275));
 
         Text txt = new Text("Sup?");
-        txt.setY(50);
-        txt.setX(10);
 
-        root.getChildren().add(txt);
+        TextField inputField = new TextField();
+
+        Button btn  = new Button();
+        btn.setText("Say sup!");
+        btn.setOnAction(evt -> System.out.printf("Sup %s?%n", inputField.getText()));
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(txt, inputField, btn);
+
+        root.getChildren().add(vBox);
 
         primaryStage.show();
     }
